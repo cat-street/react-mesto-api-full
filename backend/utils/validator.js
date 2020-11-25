070ee3404aae38d6682280c22de958edcd92201f
Andrey Kudryavtsev <lomastering@gmail.com>
2020-11-25 19:02:58 +0300

    fix: валидация ссылок

--- backend/utils/validator.js
+++ backend/utils/validator.js
@@ -2,6 +2,6 @@ const isEmail = require('validator/lib/isEmail');
 
 module.exports.urlValidator = (v) =>
   // eslint-disable-next-line implicit-arrow-linebreak
-  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*$)/.test(v);
+  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.,~#?&//=!]*$)/.test(v);
 
 module.exports.emailValidator = (v) => isEmail(v);
